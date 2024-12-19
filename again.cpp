5de65c1db94d092dd3982943e1796ffabcef0e8d
SAIF-tech34 <154990938+SAIF-tech34@users.noreply.github.com>
2024-12-19 16:01:34 +0500

    Add files via upload

---
+++ again.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int a;
+	cout << "I don't use any other compiler\n";
+	cin  >> a;
+	return 0;
+}
---
+++ another string.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+	string userName = "Saif ullah";
+	string sirName = "Rais";
+	string fullName = userName + sirName;
+	cout << fullName;
+	return 0;
+}
