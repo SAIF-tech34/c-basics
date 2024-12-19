f09f99a5efb10134ea7542f0655386f50b9dd2c5
SAIF-tech34 <154990938+SAIF-tech34@users.noreply.github.com>
2024-12-19 14:51:33 +0500

    Add files via upload

---
+++ 1for1loop.cpp
@@ -0,0 +1,13 @@
+// program that inputs message and line will decide on user's promt input
+#include <iostream>
+using namespace std;
+int main () {
+	cout << "Input number :\n";
+	int n;
+	cin >> n;
+// syntax: for(initialization conditioin;continuation condition; incrementing condition)
+	for (int i = 1; i <= n; i++) {
+		cout << "I am sorry for all\n";
+}
+	return 0;
+}
