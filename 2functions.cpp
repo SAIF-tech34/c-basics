10d57b77320a00606c89e607c9731f2b1d2ed9cd
SAIF-tech34 <154990938+SAIF-tech34@users.noreply.github.com>
2024-12-19 14:58:58 +0500

    Add files via upload

---
+++ 2functions.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+// initialize function like initialize variable before declaration
+void function();
+int main() {
+     function();
+	return 0;
+} 
+  void function() {
+  	cout << "Programming in c++";
+  }
